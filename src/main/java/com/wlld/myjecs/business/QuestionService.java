b2e8f5c6383b1e0a77d94544a8d5e80839fc31c1
laowang777 <laowang@qq.com>
2024-03-12 23:20:01 +0800

    增加admin权限问答界面

---
+++ src/main/java/com/wlld/myjecs/business/QuestionService.java
@@ -0,0 +1,30 @@
+package com.wlld.myjecs.business;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.wlld.myjecs.mapper.QaMapper;
+import com.wlld.myjecs.sqlEntity.Qa;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+/**
+ * @author Laowang
+ */
+@Slf4j
+@Service
+public class QuestionService {
+
+
+    @Autowired
+    private QaMapper qaMapper;
+
+    public PageInfo<Qa> findAll(Integer pageIndex, Integer pageSize) {
+        PageHelper.startPage(pageIndex, pageSize);
+        List<Qa> all = qaMapper.findAll(pageIndex, pageSize);
+        return new PageInfo<>(all);
+    }
+}
