17cbcde607492aec1f1e90c11c72d287fa67e44b
17679318598 <442232278@qq.com>
2024-05-09 18:57:24 +0800

    优化websocket回复

--- src/main/java/com/wlld/myjecs/service/SentenceService.java
+++ src/main/java/com/wlld/myjecs/service/SentenceService.java
@@ -1,7 +1,10 @@
 package com.wlld.myjecs.service;
 
+import com.wlld.myjecs.entity.KeywordSql;
 import com.wlld.myjecs.entity.Sentence;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
 * @author 44223
@@ -9,5 +12,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2024-03-14 10:30:35
 */
 public interface SentenceService extends IService<Sentence> {
-
+    List<Sentence> listByKeyWord(KeywordSql query);
+    List<Sentence> listByOrders(KeywordSql query, List<Integer> ktIds);
 }
