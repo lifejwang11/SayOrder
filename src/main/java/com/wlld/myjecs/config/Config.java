f77d1f0f0e451296931c4abd89cf2df406d980e1
fushoujiang <fushoujiang@daojia-inc.com>
2024-12-17 16:17:30 +0800

    feat(datasource): 添加华为云 GaussDB 数据源配置
    - 新增 application-huawei.yml 配置文件，用于连接华为云 GaussDB 数据库
    - 修改 application.yml，设置华为云配置为默认数据源
    - 更新 Config 类，使用系统类加载器获取模型文件路径- 在 pom.xml 中添加 GaussDB JDBC 驱动依赖

--- src/main/java/com/wlld/myjecs/config/Config.java
+++ src/main/java/com/wlld/myjecs/config/Config.java
@@ -1,14 +1,18 @@
 package com.wlld.myjecs.config;
 
-public class Config {//配置文件 "/usr/local/depends/
-    public static final String Word2VecModelUrl = "/Users/lidapeng/job/sayOrderModel/end.json";//词向量嵌入模型（注意词向量模型无需频繁更新，只有当样本有超过50%增量时才有更新必要）
-    public static final String SentenceModelUrl = "/Users/lidapeng/job/sayOrderModel/sentence.json";//语义分辨模型
-    public static final String onlyKeyWord = "/Users/lidapeng/job/sayOrderModel/preKeyWord.json";//关键词敏感性嗅探
-    public static final String KeyWordModelUrl = "/Users/lidapeng/job/sayOrderModel/myKeyWord.json";//关键词查找模型
-    public static final String keyWordIndex = "/Users/lidapeng/job/sayOrderModel/keyWordIndex.json";//关键词索引模型
-    public static final String wordUrl = "/Users/lidapeng/job/sayOrderModel/word.json";//聊天词向量嵌入模型
-    public static final String longTalkUrl = "/Users/lidapeng/job/sayOrderModel/longTalk.json";//聊天问答模型
-    public static final String shortTalkUrl = "/Users/lidapeng/job/sayOrderModel/shortTalk.json";//聊天问答模型
+
+public class Config {
+    public static final String path = ClassLoader.getSystemClassLoader().getResource("").getPath();
+
+
+    public static final String Word2VecModelUrl = path  +"/end.json";//词向量嵌入模型（注意词向量模型无需频繁更新，只有当样本有超过50%增量时才有更新必要）
+    public static final String SentenceModelUrl = path  +"/sentence.json";//语义分辨模型
+    public static final String onlyKeyWord = path  +"/preKeyWord.json";//关键词敏感性嗅探
+    public static final String KeyWordModelUrl = path  +"/myKeyWord.json";//关键词查找模型
+    public static final String keyWordIndex = path  +"/keyWordIndex.json";//关键词索引模型
+    public static final String wordUrl = path  +"/word.json";//聊天词向量嵌入模型
+    public static final String longTalkUrl = path  +"/longTalk.json";//聊天问答模型
+    public static final String shortTalkUrl = path  +"/shortTalk.json";//聊天问答模型
     public static final boolean selfTest = false;//服务启动时是否需要自检（自检时间较长）
     public static final boolean starModel = true;//服务启动是否启动模型
     public static final String adminAccount = "admin";//管理员账号
